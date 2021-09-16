798762c4e9c78cfb0b951d5f9fbd344ec3ef2052
angelbritoFI <abs99@comunidad.unam.mx>
2021-09-16 00:29:03 -0500

    Provando frecuencias altas en simulador

---
+++ Programa_1/bisa_pwm.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# Author: Brito Segura Angel
+# Version 1.0
+# Date: 13/09/2021
+# Description: Papadear un led utilizando un modulador de pulso de la Raspberry Pi
+# Físicamente, el led está conectado al pin 32 mediante un Array Darlington ULN2003
+
+# ## ###############################################
+# Fundamentos de Sistemas Embebidos, Semestre 2022-1
+# -*- coding: utf-8 -*-
+# Autor: Mauricio Matamoros
+# License: MIT
+# ## ###############################################
+
+# Importación de future (para compatibilidad con Python 2.7)
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Importación de la librería de control del GPIO de la Raspberry Pi
+import RPi.GPIO as GPIO
+# Importación de la función sleep del módulo time para control mediante tiempos de espera
+from time import sleep
+
+# Inicialización de placa virtual (comentar si es implementación en hardware)
+import virtualboard
+# Desactiviar advertencias (descomentar si es implementación en hardware)
+# GPIO.setwarnings(False)
+
+#Configuraciones de la librería Rpi.GPIO
+GPIO.setmode(GPIO.BOARD) # Usar el número físico de pin
+GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW) # Habilitar pin 32: como de salida y en bajo
+pwm = GPIO.PWM(32, 50000) # Inicializar pin 32 como PWM a una frecuencia de 50000 Hz
+
+print("Iniciando PWM")
+pwm.start(50) # Establecer ciclo de trabajo al 50% para que parpadeé cada 500 ms
+print("PWM iniciado")
+flag = True
+
+# Ciclo infinito para parpadear un led hasta que ocurra una excepción
+while flag:
+	try:
+		dutyCycle = int(input("Ingrese ciclo de trabajo: "))
+		pwm.ChangeDutyCycle(dutyCycle)
+	# Detener con el shortcut CTRL+C o con excepción
+	except:
+		flag = False
+		print("Valor no permitido :(")
+		pwm.ChangeDutyCycle(0)
+		print("Error: ", exc_info()[0])
+
+# Detener PWM
+pwm.stop()
+print("PWM terminado")
+
+# Reiniciar todos los puertos para que estén en su estado por defecto (entradas)
+GPIO.cleanup()
